73fa2866e7aca3ee0f5c3b97bb62841136a203c0
ikngoo <60819389+ikngoo@users.noreply.github.com>
2021-06-06 13:36:41 +0800

    2nd

--- ScrapeOD115950.R
+++ ScrapeOD115950.R
@@ -3,17 +3,11 @@
 library(tidyverse)
 library(jsonlite)
 
-#nse top gainers
+#get csv download url
 
-url <- 'https://data.gov.tw/api/v2/rest/dataset/115950'
+url <- 'https://quality.data.gov.tw/dq_download_csv.php?nid=115950&md5_url=1f49e6e3e77ba203a4bba37f3b263715'
 
-# extract html 
-
-url_html <- read_json(url)
-
-lastdis = length(url_html$result$distribution)
-
-csvurl = url_html$result$distribution[[lastdis]]$resourceDownloadUrl
+# read csv 
 
 df <- read.table(file = csvurl, header=TRUE, sep=",", fileEncoding = "UTF-8-BOM", fill=TRUE)
 
